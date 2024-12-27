4ad14a08e107381e6bba1f0914cd553479946ecf
Hampter <Bradbeeschwarz@gmail.com>
2024-12-26 23:58:52 -0800

    Create __init__.py

---
+++ parser/__init__.py
@@ -0,0 +1,4 @@
+from .parser import Parser  # Import the Parser class to make it available at the package level
+
+# You can also import other functions or classes from parser.py if needed
+# from .parser import some_other_function_or_class
