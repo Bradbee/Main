1012dd6e9dcca64c58be104dffef907ad1765a93
Hampter <Bradbeeschwarz@gmail.com>
2024-12-27 00:10:34 -0800

    Create __init__.py

---
+++ interpreter/__init__.py
@@ -0,0 +1,6 @@
+from .interpreter import Interpreter
+
+# You can also import other utility functions or classes here if needed
+# For example, if you have specific helpers or built-ins, you could include them as well.
+
+# This file makes the Interpreter class easily accessible
