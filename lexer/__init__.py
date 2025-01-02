f0e9d1c2409dcff52f38b5a10212e5b612b20d45
Hampter <Bradbeeschwarz@gmail.com>
2025-01-01 20:32:13 -0800

    Delete lexer directory

--- lexer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lexer import Lexer  # Import the Lexer class to make it available when the lexer package is imported
-
-# You can also import other functions or classes from lexer.py if needed
-# from .lexer import some_other_function_or_class
