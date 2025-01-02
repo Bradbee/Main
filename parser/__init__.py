a9d40e15c20c8f5d9bba013f8c476c10ead3d607
Hampter <Bradbeeschwarz@gmail.com>
2025-01-01 20:31:59 -0800

    Delete parser directory

--- parser/__init__.py
+++
@@ -1,4 +0,0 @@
-from .parser import Parser  # Import the Parser class to make it available at the package level
-
-# You can also import other functions or classes from parser.py if needed
-# from .parser import some_other_function_or_class
